acc85ae94a49aae7377b1e3e253f88c3f4471471
aviv-segal <avivsegal2008@gmail.com>
2025-09-09 12:49:57 +0300

    users

--- src/routes/users.rs
+++ src/routes/users.rs
@@ -1,10 +1,27 @@
 use gurt::{GurtResponse, Result};
 use sqlite::Connection;
 
+#[derive(Debug)]
+struct User {
+    name: String,
+    password: String,
+    currency: i32,
+    create: bool
+}
+
 pub fn get(body: String) -> Result<GurtResponse> {
+    let db = Connection::open("gambling.db").unwrap();
+    let query = format!("SELECT * FROM users WHERE name = '{}'", body);
     Ok(GurtResponse::ok().with_string_body(""))
 }
 
 pub fn post(body: String) -> Result<GurtResponse> {
+    let db = Connection::open("gambling.db").unwrap();
+    let user_data: Vec<&str> = body.split(',').collect();
+    let query = format!(
+        "INSERT INTO users (name, password, currency) VALUES ('{}', '{}', '{}')",
+        user_data[0], user_data[1], user_data[2]
+    );
+    db.execute(query).unwrap();
     Ok(GurtResponse::ok().with_string_body(String::from("User created")))
 }
