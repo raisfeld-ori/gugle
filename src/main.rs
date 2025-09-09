5ee7cfc28bafae14b870090a693d06ea691f0819
ori raisfeld <raisfeldori@gmail.com>
2025-09-09 10:38:54 +0300

    Add slots route and HTML response handling

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,8 @@ async fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
     
     let server = GurtServer::with_tls_certificates("cert.pem", "key.pem")?
-        .get("/", |_ctx| async { routes::index()});
+        .get("/", |_ctx| async { routes::index()})
+        .get("/slots", |_ctx| async { routes::slots() });
 
     println!("GURT server starting on gurt://127.0.0.1:4878");
     server.listen("127.0.0.1:4878").await
--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,3 +1,4 @@
 mod index;
-
+mod slots;
 pub use index::index;
+pub use slots::index as slots;
