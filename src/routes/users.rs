71cc72b1a2de71cde0df105bd501b260a6fbd533
Fravy <099558er@gmail.com>
2025-09-09 11:54:57 +0300

    minor fix (I like minors)

--- src/routes/users.rs
+++ src/routes/users.rs
@@ -1,10 +1,10 @@
 use gurt::{GurtResponse, Result};
 use sqlite::Connection;
 
-pub fn get(body: string) -> Result<GurtResponse> {
+pub fn get(body: String) -> Result<GurtResponse> {
     Ok(GurtResponse::ok().with_string_body(""))
 }
 
-pub fn post(body: string) -> Result<GurtResponse> {
+pub fn post(body: String) -> Result<GurtResponse> {
     Ok(GurtResponse::ok().with_string_body(String::from("User created")))
 }
